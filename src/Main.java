2cfa78e586712cea968b964ef8e11fec72ca0c4b
Abolina667 <abai.sagizhan.absi@gmail.com>
2023-04-06 09:49:02 +0600

    he function that finds average of numbers

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,30 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        int[] arr = {3, 2, 4, 1};
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for(int i = 0; i<n; i++) {
+            arr[i] = sc.nextInt();
+        }
         double average = findAverage(arr, arr.length);
         System.out.println("Average is " + average);
     }
 
+    /**
+     * findAverage() - this function calculates the average of the numbers
+     * @param arr - array of elements, that contains numbers to find average
+     * @param n - size of array
+     * @return - return average value
+     */
     public static double findAverage(int[] arr, int n) {
         double sum = 0;
         if (n == 1)
             return arr[0];
         else{
-            for(int i = 0; i < n;i++){
-                sum += arr[i];
-            }
-            return sum/n;
+            sum = findAverage(arr, n-1)*(double)(n-1);
+            sum += arr[n-1];
+            return sum / (double)n;
         }
     }
 }
