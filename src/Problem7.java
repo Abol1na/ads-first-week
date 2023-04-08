86bb32a40f4fd1245e0452260cdfd5be267c4f32
Abolina667 <abai.sagizhan.absi@gmail.com>
2023-04-09 00:17:16 +0600

    This function to reverse order of given array

--- src/Problem7.java
+++ src/Problem7.java
@@ -3,6 +3,7 @@ public class Problem7 {
     public static void main(String[] args){
         Scanner scanner = new Scanner(System.in);
         int n = scanner.nextInt();
-        System.out.println(n);
+        int m = scanner.nextInt();
+        System.out.println(n + " " + m);
     }
 }
