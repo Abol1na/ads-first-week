1100821f974bf5fc04b63c9a78d1b39ef10b05f0
Abolina667 <abai.sagizhan.absi@gmail.com>
2023-04-08 22:46:14 +0600

    This function calculates binomial coefficient

--- src/Problem7.java
+++ src/Problem7.java
@@ -1,2 +1,15 @@
+import java.util.Scanner;
 public class Problem7 {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int k = scanner.nextInt();
+        System.out.println(findBinom(n,k));
+    }
+    public static int findBinom(int n, int k){
+        if(n == k || k == 0)
+            return 1;
+        else
+            return findBinom(n-1,k-1) + findBinom(n-1,k);
+    }
 }
