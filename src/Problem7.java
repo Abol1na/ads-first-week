647c956dfe3a4ba0eafc73a499aecb3cda415969
Abolina667 <abai.sagizhan.absi@gmail.com>
2023-04-09 00:41:32 +0600

    This function to reverse order of given array

--- src/Problem7.java
+++ src/Problem7.java
@@ -1,14 +1,21 @@
 import java.util.Scanner;
 public class Problem7 {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int n = scanner.nextInt();
         int[] arr = new int[n];
-        for(int i = 0; i < n; i++){
+        for (int i = 0; i < n; i++) {
             arr[i] = scanner.nextInt();
         }
-        for(int i = n-1; i >= 0; i--){
+        arrReverse(arr, 0, n - 1);
+        for (int i = 0; i < n; i++) {
             System.out.print(arr[i] + " ");
         }
+    }
+    public static void arrReverse(int[] arr, int a, int b){
+        int c = arr[a];
+        arr[a] = arr[b];
+        arr[b] = c;
+        arrReverse(arr, a+1, b-1);
     }
 }
